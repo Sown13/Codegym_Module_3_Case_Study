834299c6d617872165b811d5ec9980f7c2e97063
Sown13 <sonnh.buh@gmail.com>
2023-05-24 09:44:03 +0700

    upload sql database

---
+++ sql/init_db.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE case_study_m3;
+use case_study_m3;
+
+CREATE TABLE users (
+u_id int auto_increment primary key,
+user_name varchar(50) not null unique,
+password varchar(50) not null,
+full_name varchar(255),
+address varchar(255),
+email varchar(255) not null unique
+);
+
+Create table songs (
+s_id int auto_increment primary key,
+song_name varchar(255) not null,
+author varchar(255),
+upload_day timestamp default current_timestamp,
+url text not null,
+listening_frequency int,
+label varchar(50),
+u_id int,
+foreign key (u_id) references users(u_id)
+);
+
+create table playlist (
+p_id int auto_increment primary key,
+p_name varchar(255) not null,
+u_id int,
+foreign key (u_id) references users(u_id)
+);
+
+create table likes (
+u_id int,
+s_id int,
+foreign key(u_id) references users(u_id),
+foreign key(s_id) references songs(s_id),
+primary key (u_id, s_id)
+);
+
+create table comments (
+comment_id int auto_increment primary key,
+content text,
+s_id int,
+u_id int,
+foreign key (u_id) references users(u_id),
+foreign key (s_id) references songs(s_id)
+);
+
+create table playlist_detail (
+pd_id int primary key auto_increment,
+s_id int,
+p_id int,
+foreign key (s_id) references songs(s_id),
+foreign key (p_id) references playlist(p_id)
+);
+
+
